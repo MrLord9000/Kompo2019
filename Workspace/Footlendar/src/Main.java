26933ddcf5fc8988658830865c7ade8dfc0bfb4e
Adrian Zieliński <adrianziel24@wp.pl>
2019-05-27 17:48:22 +0200

    sqlserver

--- Workspace/Footlendar/src/Main.java
+++ Workspace/Footlendar/src/Main.java
@@ -1,23 +1,72 @@
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+public class Main 
+{
 
-import controller.ConsoleNotifier;
-import controller.Match;
-import controller.Team;
-import controller.User;
-
-public class Main {
-
-	public static void main(String[] args) {
-		System.out.println("xD");
-		User.getInstance().setNotifier(new ConsoleNotifier());
-		Match m = new Match(1, new Team("t1"), new Team("t1"), new GregorianCalendar(2019, Calendar.MAY, 26, 16, 50, 30), "jakis opis");
-		Match.setminutesRemindBeforeStart(1);
-		m.resetTimer();
-		User.getInstance().addTrackedMatch(m);
-		//Team t = new Team("");
+	
+	public static void print(Statement stat)
+	{
+		ResultSet rs =
+		null;
+		try
+		{
+			rs = stat.executeQuery("SELECT * FROM Teams");
+		} catch (SQLException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try
+		{
+			while(rs.next())
+			{
+				System.out.println(rs.getString("name"));
+			}
+		} catch (SQLException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+	
+	public static void main(String[] args)
+	{
+		try 
+		{
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();
+			Connection con = DriverManager.getConnection("jdbc:sqlserver://DESKTOP-41IQBFQ\\WINCCPLUSMIG2014;databaseName=FootlendarDB;integratedSecurity=true");
+			System.out.println("xD");
+			Statement stat = con.createStatement();
+			ResultSet rs = stat.executeQuery("SELECT * FROM Teams");
+			//print(stat);
+			//rs = stat.executeQuery("INSERT INTO Teams VALUES ('Argentina')");
+			print(stat);
+			
+			
+		}
+		catch(SQLException e)
+		{
+			System.out.println(e.getMessage());
+		} 
+		catch (InstantiationException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
+	}
+	
 }
