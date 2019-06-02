b3a61e557d98f6927d4686f063f76cc226064797
MrLord9000 <filip1209348756@gmail.com>
2019-06-02 23:29:02 +0200

    Finished basic calendar display and added basic event labels

---
+++ Workspace/Footlendar/src/view/gui/CalendarWindowOutOfBoundsException.java
@@ -0,0 +1,11 @@
+package view.gui;
+
+public class CalendarWindowOutOfBoundsException extends Exception
+{
+	private static final long serialVersionUID = 857124230382465713L;
+
+	public CalendarWindowOutOfBoundsException(String msg)
+	{
+		super(msg);
+	}
+}
